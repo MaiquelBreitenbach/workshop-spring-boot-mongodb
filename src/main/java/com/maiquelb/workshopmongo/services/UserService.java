adc910844695d9dc820e83cbc3d368b3ba7f4ba1
Maiquel Breitenbach <maiquel.breitenbach@gmail.com>
2021-07-17 15:14:22 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/maiquelb/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.maiquelb.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maiquelb.workshopmongo.domain.User;
+import com.maiquelb.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
